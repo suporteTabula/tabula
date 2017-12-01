2e9e1d05cdbc06e0686f93fb918ccc9d05e4d5bb
Jean Pavan <jnopavan@gmail.com>
2017-12-01 14:41:27 -0200

    Index dos grupos
    Criação: View
    Alteração: Controller, Model, Rotas, Layout da Home (Menu Lateral)

--- app/UserGroup.php
+++ app/UserGroup.php
@@ -13,6 +13,6 @@ class UserGroup extends Model
 
     public function leader()
     {
-    	return $this->belongsTo('App\User');
+    	return $this->belongsTo('App\User', 'user_id');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -34,3 +34,8 @@ Route::get('/admin/categories', 'CategoriesController@index')->name('categories'
 Route::get('/admin/category/create', 'CategoriesController@create')->name('category.create');
 Route::post('/admin/category/store', 'CategoriesController@store')->name('category.store');
 Route::get('/admin/category/delete/{id}', 'CategoriesController@destroy')->name('category.delete');
+
+Route::get('/admin/userGroups', 'UserGroupsController@index')->name('userGroups');
+Route::get('/admin/userGroups/create', 'UserGroupsController@create')->name('userGroups.create');
+Route::get('/admin/userGroups/store', 'UserGroupsController@store')->name('userGroups.store');
+Route::get('/admin/userGroups/destroy', 'UserGroupsController@destroy')->name('userGroups.destroy');
