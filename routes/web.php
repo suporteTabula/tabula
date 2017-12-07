8f722b42c24153373fce60357a6a6b311dcb9b3d
Jean Pavan <jnopavan@gmail.com>
2017-12-07 19:55:56 -0200

    Tentativa de resolver os conflitos de merge 2
    Adicionei manualmente web.php que ainda estava faltando

--- routes/web.php
+++ routes/web.php
@@ -24,6 +24,8 @@ Route::get('/admin/users', 'UsersController@index')->name('users');
 Route::get('/admin/user/delete/{id}', 'UsersController@destroy')->name('user.delete');
 Route::get('/admin/user/create', 'UsersController@create')->name('user.create');
 Route::post('/admin/user/store', 'UsersController@store')->name('user.store');
+Route::get('/admin/user/edit/{id}', 'UsersController@edit')->name('user.edit');
+Route::post('/admin/user/update/{id}', 'UsersController@update')->name('user.update');
 
 Route::get('/admin/usersType', 'UsersTypeController@index')->name('usersType');
 Route::get('/admin/usersType/create', 'UsersTypeController@create')->name('userType.create');
@@ -33,4 +35,20 @@ Route::get('/admin/userType/delete/{id}', 'UsersTypeController@destroy')->name('
 Route::get('/admin/categories', 'CategoriesController@index')->name('categories');
 Route::get('/admin/category/create', 'CategoriesController@create')->name('category.create');
 Route::post('/admin/category/store', 'CategoriesController@store')->name('category.store');
+Route::get('/admin/category/edit/{id}', 'CategoriesController@edit')->name('category.edit');
+Route::post('/admin/category/update/{id}', 'CategoriesController@update')->name('category.update');
 Route::get('/admin/category/delete/{id}', 'CategoriesController@destroy')->name('category.delete');
+
+Route::get('/admin/userGroups', 'UserGroupsController@index')->name('userGroups');
+Route::get('/admin/userGroups/create', 'UserGroupsController@create')->name('userGroups.create');
+Route::post('/admin/userGroups/store', 'UserGroupsController@store')->name('userGroups.store');
+Route::get('/admin/userGroups/edit/{id}', 'UserGroupsController@edit')->name('userGroups.edit');
+Route::post('/admin/userGroups/update/{id}', 'UserGroupsController@update')->name('userGroups.update');
+Route::get('/admin/userGroups/destroy/{id}', 'UserGroupsController@destroy')->name('userGroups.destroy');
+
+Route::get('/admin/companies', 'CompaniesController@index')->name('companies');
+Route::get('/admin/companies/create', 'CompaniesController@create')->name('companies.create');
+Route::post('/admin/companies/store', 'CompaniesController@store')->name('companies.store');
+Route::get('/admin/companies/edit/{id}', 'CompaniesController@edit')->name('companies.edit');
+Route::post('/admin/companies/update/{id}', 'CompaniesController@update')->name('companies.update');
+Route::get('/admin/companies/destroy/{id}', 'CompaniesController@destroy')->name('companies.destroy');
