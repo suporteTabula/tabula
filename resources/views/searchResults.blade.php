5c23adda3d2d79e0628fc5706226ab05a92ab1c8
Rafahur <rfahur@gmail.com>
2018-04-27 11:47:48 -0300

    Mudanças de botões e inputs.

--- resources/views/searchResults.blade.php
+++ resources/views/searchResults.blade.php
@@ -1,7 +1,7 @@
 @if (isset($courses))
 	@if (count($courses) > 0)
         @foreach($courses as $course)
-            <a href="{{ route('course.single', ['id' => $course->id]) }}" class="column col-4 col-xs-12 col-sm-12 col-md-4 col-lg-3 col-xl-3 course-item">
+            <a href="{{ route('course.single', ['id' => $course->id]) }}" class="column col-3 col-xs-12 col-sm-12 col-md-4 col-lg-3 col-xl-3 course-item">
                 <div class="columns">
                     <div class="column col-xs-4 col-sm-4 col-md-12 col-xl-12 col-lg-12 course-image" style="background-image: url(../images/aulas/{{$course->thumb_img}});"></div>
                     <div class="column col-xs-8 col-sm-8 col-md-12 col-xl-12 col-lg-12 course-content bg-primary-gray text-white">
