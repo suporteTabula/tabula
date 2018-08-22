74c46cd579b582d21f5307f11e44842ca7278f01
Rafahur <rfahur@gmail.com>
2018-08-22 12:23:28 -0300

    prefixers fix 2

--- resources/views/todosProfs.php
+++ resources/views/todosProfs.php
@@ -23,15 +23,9 @@
             <div class="columns">
                 <div class="nav-brand column col-2 col-xs-10 col-sm-6 col-md-3 col-lg-3 col-xl-2">
                     <div class="dropdown">
-<<<<<<< HEAD
                     <span><a href="/"><img align="center" src="images/layout/header/logo.png" height="30px"> </a></span>
                     <div class="dropdown-content">
                     <a href="/homeEmpresa"> <img src="images/layout/header/sp.jpg" height="25px"></a>
-=======
-                    <span><a href="/home1"><img align="center" src="images/layout/header/logo.png" height="30px"> </a></span>
-                    <div class="dropdown-content">
-                    <a href="/"> <img src="images/layout/header/sp.jpg" height="25px"></a>
->>>>>>> navbar
                     </div>
                     </div>
                 </div>
