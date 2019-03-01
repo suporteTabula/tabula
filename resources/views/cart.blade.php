092ef082c1dc4f2be4b97b9cf815016b9ee01ebf
negothales97 <thales.serra97@gmail.com>
2019-03-01 18:42:52 -0300

    layout checkout

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -50,20 +50,20 @@
         <div class="container grid">
             <div class="column checkout-courses">
                 <div class="columns">                                                          
-                    <div class="column col-6 check-card">                    
+                    <div class="column col-xs-12 col-sm-12 col-md-12 col-lg-6 col-xl-6 check-card">                    
                         @foreach(array_chunk($courses,2) as $chunks)
                         <div class="columns">
                             @foreach($chunks as $course)
-                            <div class="column col-6 two">
+                            <div class="column col-xs-12 col-sm-12 col-6 col-lg-6 col-xl-6 two">
                                 <div class="columns course-cart-item">
                                     <div class="column col-4 check-img"></div>  
                                     <div class="column col-5 three">
                                         <p class="course-cart-title">{{ $course->name }}</p><br/>
                                         <p>{{ $course->desc }}</p><br/>
-                                        <p>{{ $course->price }}</p>                                                
+                                        <p>R$ {{ $course->price }}</p>                                                
                                     </div>
                                     <div class="column col-1"></div>           
-                                    <div class="column col-2 course-target-remove-container">
+                                    <div class="column col-2 course-target-remove-container remove-curso">
                                         <a href="{{ route('cart.remove', ['id' => $course->cart_id]) }}" class="course-cart-remove">X</a>
                                     </div>
                                 </div>
@@ -72,21 +72,21 @@
                         </div>
                         @endforeach                        
                     </div>
-                    <div class="column col-6 check-card"><!-- Master Column Right  action="{{route('cart.cupom')}}-->
+                    <div class="column col-xs-12 col-sm-12 col-md-12 col-lg-6 col-xl-5 check-card"><!-- Master Column Right  action="{{route('cart.cupom')}}-->
                         <div class="column col-12 total-checkout">
                             <p class="checkout-title">SubTotal</p>
                             <span >R${{ number_format($total_price, 2,',', '.') }}</span>
 
-                                <div class="col-sm-8 my-1">
+                                <div class="my-1">
                                     <label class="sr-only" for="codCupom">Cupom</label>
                                     <input type="text" class="form-control " name="codCupom" id="codCupom" placeholder="Informar Cupom">
                                     <button class="button-tabula-gray" id="cupom">Validar</button>
                                 </div>
                             <p class="checkout-title">Total</p>
                             <span id="total">R${{number_format($total_price, 2, ',', '.')}}</span>
-                            <a href="{{ route('cart.checkout') }}" class="button-tabula-gray">Finalizar Compra</a>
+                            <a id="btn-finalizar" href="{{ route('cart.checkout') }}" class="button-tabula-gray">Finalizar Compra</a>
 
-                            <a href="{{ route('search.single', ['id' => -1]) }}" class="button-tabula-gray">Continuar</a>
+                            <a id="btn-continuar" href="{{ route('search.single', ['id' => -1]) }}" class="button-tabula-gray">Continuar</a>
                         </div>
 
                     </div>
